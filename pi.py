37d9b37808454e10c7f087ee1be4760031e49b5f
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-03-14 10:46:07 +0530

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,17 @@
+n=int(1e8)
+print(n)
+s=0.0
+for k in range(n):
+    s=s+(((-1.0)**k)/(2.0*k+1.0)*4.0)
+
+print(s)
+
+"""
+100000000
+3.141592643589326
+
+Pi day 2025
+
+Code by Harish on I7 for 1 min.. Next I want supercomputer for 1 min..
+want n=1e34 in 1 min in supercomputer..
+"""
